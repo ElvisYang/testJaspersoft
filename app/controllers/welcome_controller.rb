a525d2e4c621c4d784639797b6b6c13fe8d12c31
ElvisYang <elvisya@gmail.com>
2016-08-18 10:17:53 +0800

    Init

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,31 @@
+class WelcomeController < ApplicationController
+
+	def index
+		# require 'pry'
+
+		# Pry.start
+
+		JasperserverRails::Jasperserver.new.run_report 'public/test.pdf' do
+		  format 'pdf'
+		  # report 'public/ElvisTest_Report'
+		  report 'public/Samples/Reports/01._Geographic_Results_by_Segment_Report'
+		  params({ :Value1 => 'Value1' })
+		end
+
+		JasperserverRails::Jasperserver.new.run_report 'public/test.csv' do
+		  format 'csv'
+		  # report 'public/ElvisTest_Report'
+		  report 'public/Samples/Reports/01._Geographic_Results_by_Segment_Report'
+		  params({ :Value1 => 'Value1' })
+		end
+
+		JasperserverRails::Jasperserver.new.create_organization("test") do
+
+		end
+	end
+
+	def generate
+
+	end
+
+end
